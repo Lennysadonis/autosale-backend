447046c733f5150e06ff7a819f9f36434030ee3f
ADONNNYS <adonis.davila807@gmail.com>
2026-04-16 02:13:30 -0600

    fix: corregidas entidades con ! para typescript

--- src/modules/vehicles/vehicles.module.ts
+++ src/modules/vehicles/vehicles.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { VehiclesController } from './controllers/vehicles.controller';
 import { VehiclesService } from './services/vehicles.service';
+import { vehicle } from './entities/vehicle.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([vehicle]) // 🔥 ESTA LÍNEA ES LA CLAVE
+  ],
   controllers: [VehiclesController],
   providers: [VehiclesService],
   exports: [VehiclesService],
